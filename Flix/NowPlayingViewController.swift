5f5c2839c3b94dda985520b93992204c7bfdd510
Kam Khai <patrick.phyo@gmail.com>
2018-09-05 20:38:57 -0700

    final commit

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,129 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Pat Khai on 9/5/18.
+//  Copyright © 2018 Pat Khai. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+import KRActivityIndicatorView
+import PKHUD
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+     var activityIndicator = KRActivityIndicatorView()
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.dataSource = self
+         tableView.rowHeight = 169
+        fetchMovies()
+        tableView.insertSubview(refreshControl, at: 0)
+
+        
+    
+        // Do any additional setup after loading the view.
+
+
+    }
+    
+    //for refresh when pull down the movies
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+        
+    }
+   
+   
+    
+    func fetchMovies(){
+        //network referesh
+       
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        //start the HUD
+        PKHUD.sharedHUD.contentView = PKHUDProgressView()
+        PKHUD.sharedHUD.show(onView: tableView)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) {(data, response, error) in
+            //end the HUD
+            
+            PKHUD.sharedHUD.hide(afterDelay: 1.0)
+            
+            //Network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                //parse the API network and turn it into dictionary
+                let dataDictionary =  try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any] //cast dictionary[]
+                //will get all the movies title in the api
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                //need this becsaue the system is call the Network api very fast
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+//               self.activityIndicator.startAnimating()
+                
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! TableViewMovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        //get the image url from api config and actualy image path
+        let posterPathURL = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString +  posterPathURL)!
+        cell.posterImage.af_setImage(withURL: posterURL)
+       
+        
+        return cell
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/TableViewMovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewMovieCell.swift
+//  Flix
+//
+//  Created by Pat Khai on 9/5/18.
+//  Copyright © 2018 Pat Khai. All rights reserved.
+//
+
+import UIKit
+
+class TableViewMovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
